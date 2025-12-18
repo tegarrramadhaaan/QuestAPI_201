2a07a37d1cc44880f3a99c53b07128ab463e12b9
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-18 20:36:33 +0700

    menambahkan code DestinasiHome.kt

--- app/src/main/java/com/example/mydatasiswa/navigasi/DestinasiHome.kt
+++ app/src/main/java/com/example/mydatasiswa/navigasi/DestinasiHome.kt
@@ -2,3 +2,7 @@ package com.example.mydatasiswa.navigasi
 
 import com.example.mydatasiswa.R
 
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
