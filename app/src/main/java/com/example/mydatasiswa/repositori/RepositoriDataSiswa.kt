11df5bceaf55f031898e946b1e72dbd72bd2ffb2
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-18 20:39:20 +0700

    menambahkan code RepositoriDataSiswa.kt

--- app/src/main/java/com/example/mydatasiswa/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/mydatasiswa/repositori/RepositoriDataSiswa.kt
@@ -1,23 +1,2 @@
 package com.example.mydatasiswa.repositori
 
-import com.example.mydatasiswa.apiservice.ServiceApiSiswa
-import com.example.mydatasiswa.modeldata.DataSiswa
-
-interface RepositoriDataSiswa {
-    suspend fun getDataSiswa() : List<DataSiswa>
-
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-// suspend fun getSatuSiswa(id:Int) : DataSiswa
-// suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) retrofit2.Response<Void>
-// suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
-}
-
-class JaringanRepositoriDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-):RepositoriDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-// override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
-// override suspend fun
-// override suspend fun
-}
