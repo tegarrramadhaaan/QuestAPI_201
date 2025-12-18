8e39da4ce08a90abdaea54efda9e355668c90412
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-18 20:33:01 +0700

    menambahkan code service api siswa

---
+++ app/src/main/java/com/example/mydatasiswa/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.mydatasiswa.apiservice
+
+import com.example.mydatasiswa.modeldata.DataSiswa
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+
+}
