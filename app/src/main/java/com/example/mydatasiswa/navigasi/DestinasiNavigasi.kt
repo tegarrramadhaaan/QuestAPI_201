14f70d4f39d7576dee3a15c2cb82a00b016174a5
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-18 20:37:22 +0700

    menambahkan code DestinasiNavigasi.kt

--- app/src/main/java/com/example/mydatasiswa/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/mydatasiswa/navigasi/DestinasiNavigasi.kt
@@ -1,2 +1,19 @@
 package com.example.mydatasiswa.navigasi
 
+import androidx.annotation.StringRes
+
+/**
+ * Interface to declare navigation properties for a screen.
+ */
+interface DestinasiNavigasi {
+    /**
+     * Unique name to define the path for a composable
+     */
+    val route: String
+
+    /**
+     * String resource id to be displayed in the top bar
+     */
+    @get:StringRes
+    val titleRes: Int
+}
