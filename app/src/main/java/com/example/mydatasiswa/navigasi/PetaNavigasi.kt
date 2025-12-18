609c5070ce6a04f8c14fdf94b8de4cb6692c6d94
tegarrramadhaaan <anakrama2000@gmail.com>
2025-12-18 20:38:11 +0700

    menambahkan code PetaNavigasi.kt

--- app/src/main/java/com/example/mydatasiswa/navigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/mydatasiswa/navigasi/PetaNavigasi.kt
@@ -1,2 +1,35 @@
 package com.example.mydatasiswa.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mydatasiswa.ui.halaman.DestinasiHome
+import com.example.mydatasiswa.ui.halaman.EntrySiswaScreen
+import com.example.mydatasiswa.ui.halaman.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController()) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
